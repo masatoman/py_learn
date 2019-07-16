13856defd2c47f02c9d039a34514d5a3f23f16eb
geek-mihara <mihara@geek.co.jp>
2019-07-16 19:44:26 +0900

    chapter5

---
+++ append.py
@@ -0,0 +1,2 @@
+with  open('output.txt', 'a') as append_file:
+    append_file.write('\npy!\n')
---
+++ output.py
@@ -0,0 +1,3 @@
+output_file = open('output.txt', 'w')
+output_file.write('Hello world')
+output_file.close()
